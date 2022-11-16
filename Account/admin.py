0fabb14edb3588f3f92742b489ec21c60e83a84f
Macwdo <danilo.macedofernandes@hotmail.com>
2022-11-16 15:08:53 -0300

    add tables

--- Account/admin.py
+++ Account/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Usuarios,Transferencia,Instituicao
+
 
 # Register your models here.
+
+@admin.register(Usuarios)
+class UsuariosAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Transferencia)
+class TransferenciaAdmin(admin.ModelAdmin):
+    ...
+    
+@admin.register(Instituicao)
+class InstituicaoAdmin(admin.ModelAdmin):
+    ...
+    
+
+
+
--- Account/models.py
+++ Account/models.py
@@ -5,17 +5,26 @@ from django.db import models
 class Instituicao(models.Model):
     nome_instituicao = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.nome_instituicao
 
 class Usuarios(models.Model):
     nome = models.CharField(max_length=100)
     cpf = models.CharField(max_length=11,unique=True)
     conta = models.FloatField(default=0)
-    instituicao = models.ForeignKey(Instituicao,on_delete=)
+    instituicao = models.ForeignKey(Instituicao,on_delete=models.PROTECT)
+
+
+    def __str__(self):
+        return self.nome
 
 class Transferencia(models.Model):
-    origem = models.ForeignKey(Usuarios)
+    origem = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
     destino = models.CharField(max_length=11)
     valor = models.FloatField()
     data = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.origem.nome
 
     
